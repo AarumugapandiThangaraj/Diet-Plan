0c6ba0f33404bbde33a0f16205d07c2dcba0d122
logesh-001 <logeshwaranshanmugam02@gmail.com>
2026-07-08 16:48:44 +0530

    update

--- server/admin/router.py
+++ server/admin/router.py
@@ -38,7 +38,7 @@ router = APIRouter(prefix="/api/admin", tags=["Admin Data Entry"])
 
 @router.get(
     "/cuisines",
-    tags=[""]
+    tags=["dev"],
     response_model=CuisineListResponse,
     summary="List All Cuisines",
     description="Retrieve all cuisines with optional pagination."
